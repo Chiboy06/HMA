bdfeb7babe1d6bb258e8e0362180b39d0ab4892c
Chinaza <elechukwujosh3@gmail.com>
2023-10-27 00:53:58 +0200

    modification

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,14 +16,14 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
 
-    @vite('resources/js/app.js')
+    {{-- @vite('resources/js/app.js') --}}
 
 
     <link
       href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
       rel="stylesheet"
     />
-          @vite('resources/css/app.css')
+          {{-- @vite('resources/css/app.css') --}}
 
     <link href="{{  asset('css/assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/assets/vendor/animate.css/animate.min.css') }}" rel="stylesheet" />
